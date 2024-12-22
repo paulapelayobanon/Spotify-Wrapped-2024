7d907206cebf95cdce54d7534cd076b4376e6be5
Paula Pelayo <ppelayobanon@gmail.com>
2024-12-22 17:01:43 +0100

    Add files via upload

--- Spotify Wrapped 2024.R
+++ Spotify Wrapped 2024.R
@@ -24,13 +24,11 @@ file.exists("~/Just for fun - R/Spotify Data/my_spotify_data/Spotify Account Dat
 file_content1 <- readLines("~/Just for fun - R/Spotify Data/my_spotify_data/Spotify Account Data/StreamingHistory_music_0.json")
 parsed_data1 <- fromJSON(paste(file_content1, collapse = " "))
 streaminghistory1 <- parsed_data1 %>% bind_rows()
-View(streaminghistory1)
 
 file.exists("~/Just for fun - R/Spotify Data/my_spotify_data/Spotify Account Data/StreamingHistory_music_1.json")
-file_content2 <- readLines("~/Just for fun - R/Spotify Data/my_spotify_data/Spotify Account Data/StreamingHistory_music_0.json")
+file_content2 <- readLines("~/Just for fun - R/Spotify Data/my_spotify_data/Spotify Account Data/StreamingHistory_music_1.json")
 parsed_data2 <- fromJSON(paste(file_content2, collapse = " "))
 streaminghistory2 <- parsed_data2 %>% bind_rows()
-View(streaminghistory2)
 
 ## Merge the files and check how it looks
 streaminghistorymerged <- bind_rows(streaminghistory1, streaminghistory2)
@@ -40,7 +38,7 @@ str(streaminghistorymerged)
 # 3. Clean data
 
 ## Transform the first column into a date column
-## Remove data prior to 2024-01-01 and after 2024-08-31
+## Remove data prior to 2024-01-01 and after 2024-10-31
 ## Create columns seconds and minutes
 streaminghistorymerged <- streaminghistorymerged %>%
   rename(date = endTime) %>%
@@ -55,17 +53,21 @@ str(mySpotify)
 ## Group by week
 ## Summarize by hours
 ## Reorder by date
-## Create a basic plot with some aesthetics
 streaminghours <- mySpotify %>%
+  group_by(week = floor_date(date, "week")) %>%
+  summarize(hours = sum(Minutes, na.rm = TRUE)/60) %>%
+  arrange(week)
+
+## Create a basic plot with some aesthetics 
+print(streaminghours <- mySpotify %>%
   group_by(week = floor_date(date, "week")) %>%
   summarize(hours = sum(Minutes, na.rm = TRUE)/60) %>%
   arrange(week) %>%
   ggplot(aes(x = week, y = hours)) + geom_col(aes(fill = hours)) +
   theme_minimal() +
   labs(x = "Week", y = "Hours", title = "Hours listened per week") +
-  scale_fill_gradient(low = "#1db954", high = "#1db954")
- 
-  
+  scale_fill_gradient(low = "#1db954", high = "#1db954"))  
+
 # 5. Retrieve activity per artist
 
 ## Group by artist
@@ -82,41 +84,67 @@ total_hours_artist <- hoursartist %>%
   arrange(-totalhours)
 
 ## Create the artist top 10 and arrange in descending order
-topartists <- total_hours_artist %>% top_n(10, totalhours)
-topartists <- topartists %>% arrange(-totalhours)
-topartists
+top10artists <- total_hours_artist %>%
+  top_n(10, totalhours) %>%
+  arrange(-totalhours)
+View(top10artists)
+
 
 ##Plot the top 10 in a bar chart
-topartistsplot <- topartists %>%
-  ggplot(aes(x = reorder(artistName, -totalhours), y = totalhours, group = artistName, fill = artistName)) +
+top10artistsplot <- top10artists %>%
+  ggplot(aes(x = reorder(artistName, -totalhours),
+             y = totalhours, group = artistName, fill = artistName)) +
   geom_bar(stat = "identity", show.legend = FALSE) + theme_minimal() +
   labs(title = "Top 10 artists", x = "Artist", y = "Hours") +
   theme(axis.text.x = element_text(angle = 45, hjust = 1))
-topartistsplot
+top10artistsplot
 
 # 6. Retrieve most listened songs
 
 ## Group by songs
 ## Summarize by minutes
 topminuteslistened <- mySpotify %>%
-  filter(!is.na(trackName)) %>%
-  group_by(trackName) %>%
+  filter(!is.na(trackName) &  !is.na(artistName)) %>%
+  group_by(trackName, artistName) %>%
   summarize(minutesListened = sum(Minutes, na.rm = TRUE))
 View(topminuteslistened)
 
-## Create the song top 10 and arrange in descending order
-topsongs <- topminuteslistened %>% top_n(10, minutesListened)
-topsongs <- topsongs %>% arrange(-minutesListened)
-topsongs
+## topminuteslistened is a good measurement but it doesn't tell the whole story
+## as it doesn't consider song length. Let's consider top plays
+
+## filter out NA values, group by song and artist
+## summarize by plays and arrange in descending order
+topplays <- mySpotify %>%
+  filter(trackName != "Unknown Track" & artistName != "Unknown Artist" &
+           !is.na(trackName) & !is.na(artistName)) %>%
+  group_by(trackName, artistName) %>%
+  summarise(plays = n(), .groups = "drop") %>%
+  arrange(-plays)
+View(topplays)
+
+## Create the songlengths variable
+songlengths <- mySpotify %>%
+  group_by(trackName, artistName) %>%
+  summarise(MaxPlayed = max(Seconds), .groups = "drop") %>%
+  arrange(-MaxPlayed)
+
+## Integrate songlengths into topplays
+## Arrange in descending order
+topsongs <- topplays %>%
+  left_join(songlengths, by = c("trackName", "artistName")) %>%
+  arrange(-plays)
+
+## Create the song top 10 considering song length
+top10songs <- topsongs %>% top_n(10, plays) %>%
+  select(trackName, artistName, plays, MaxPlayed)
+View(top10songs)
 
 ## Plot the top 10 in a bar chart
-topsongsplot <- topsongs %>%
-  ggplot(aes(x = reorder(trackName, minutesListened), y = minutesListened)) +
+top10songsplot <- top10songs %>%
+  ggplot(aes(x = reorder(trackName, plays), y = plays)) +
   geom_col(aes(fill = trackName)) + theme_minimal() +
-  labs(x = "Song", y = "Minutes", Title = "Top 10 most listened songs") +
-   theme(plot.margin = margin(0.5,0.5,-1,0.5, "cm"))
-topsongsplot
-
-
-
-  
+  labs(x = "Song", y = "Plays", title = "Top 10 most listened songs") +
+  coord_flip() +
+  theme(legend.position = "none") +
+  theme(plot.margin = margin(0.5,0.5,0.5,0.5, "cm"))
+top10songsplot
